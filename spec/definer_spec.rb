95c81e4e4bc4ed9b10d275b6bbe4c0f5e5421deb
Riki Montgomery <mostriki820@gmail.com>
2017-09-15 10:48:14 -0700

    passed second and third specs

--- spec/definer_spec.rb
+++ spec/definer_spec.rb
@@ -9,18 +9,18 @@ describe ('Definer') do
     end
   end
 
-  # describe ('#word') do
-  #   it ('takes the input word and returns it.') do
-  #     def_class = Definer.new({:word=> "Unicorn", :definition=> "A mythical white horse with a horn on its head."})
-  #     expect(def_class. ).to(eq("Unicorn"))
-  #   end
-  # end
-  #
-  # describe ('#definition') do
-  #   it ('takes input definition and returns it.') do
-  #     def_class = Definer.new({:word=> "Unicorn", :definition=> "A mythical white horse with a horn on its head."})
-  #     expect(def_class. ).to(eq("A mythical white horse with a horn on its head."))
-  #   end
-  # end
+  describe ('#word') do
+    it ('takes the input word and returns it.') do
+      def_class = Definer.new({:word=> "Unicorn", :definition=> "A mythical white horse with a horn on its head."})
+      expect(def_class.word).to(eq("Unicorn"))
+    end
+  end
+
+  describe ('#definition') do
+    it ('takes input definition and returns it.') do
+      def_class = Definer.new({:word=> "Unicorn", :definition=> "A mythical white horse with a horn on its head."})
+      expect(def_class.definition).to(eq("A mythical white horse with a horn on its head."))
+    end
+  end
 
 end
